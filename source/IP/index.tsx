483ea1297e475c9069f260d6d64817e433134a1e
Ricci <2405760+riccilnl@users.noreply.github.com>
2026-02-11 00:16:07 +0800

    chore: 移除源代码，只保留打包文件

--- source/IP/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Widget } from "scripting";
-import { renderIPWidget } from "./widget";
-
-async function main() {
-  const widget = await renderIPWidget();
-  await Widget.present(widget); // 直接展示 Widget
-}
-
-main();
